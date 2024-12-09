8fb0d90b2456581ea22705550f271628da6a9fc8
BeldyAdam <beldy.adam@gmail.com>
2024-12-09 13:37:19 +0100

    f1 2

--- src/main/java/hu/szamalk/Tabla.java
+++ src/main/java/hu/szamalk/Tabla.java
@@ -6,4 +6,22 @@ public class Tabla {
     private char[][] T;
     private char uresCella;
 
+    public Tabla(char üresCella) {
+        this.T = new char[8][8];
+        this.uresCella = üresCella;
+
+//        for (char[] row : T) {
+//            for (char i : row) {
+//                T[i] = üresCella;
+//            }
+//        }
+
+        for (int i = 0; i < T.length; i++) {
+            for (int j = 0; j < T[i].length; j++){
+                this.T[i][j] = uresCella;
+            }
+        }
+    }
+
+
 }
