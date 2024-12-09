2f0e4ed88ae56e6b0962888b6a30e100f7a583ec
BeldyAdam <beldy.adam@gmail.com>
2024-12-09 14:29:53 +0100

    f1 10

--- src/main/java/hu/szamalk/Main.java
+++ src/main/java/hu/szamalk/Main.java
@@ -1,13 +1,43 @@
 package hu.szamalk;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Tabla t = new Tabla('#');
+        System.out.println("4. feladat: Az üres tábla:\n" + t.megjelenit() + "\n");
+
         t.elhelyez(10);
-        System.out.println(t.megjelenit());
-        System.out.println(t.uresSor(5));
-        System.out.println(t.uresOszlop(5));
-        System.out.println(t.uresOszlopokSzama());
-        System.out.println(t.uresSorokSzama());
+        System.out.println("6. feladat: A feltötött tábla:\n" + t.megjelenit() + "\n");
+
+        System.out.println("9. feladat: Üres oszlopok és sorok száma:");
+        System.out.println("Oszlopok: " + t.uresOszlopokSzama());
+        System.out.println("Sorok: " + t.uresSorokSzama());
+
+
+        filebaIras();
+
+    }
+
+    //feladat 10.
+    public static void filebaIras() throws IOException {
+        File file = new File("tablak64.txt");
+        if (file.exists()) {
+            file.delete();
+        }
+
+        FileWriter fw = new FileWriter(file);
+        try {
+            for (int i = 1; i <= 64; i++) {
+                Tabla tF = new Tabla('*');
+                tF.elhelyez(i);
+                fw.write(tF.megjelenit() + "\n");
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        fw.close();
     }
 }
